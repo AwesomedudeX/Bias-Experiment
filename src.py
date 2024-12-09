855b5908bf286dd3fc7b5c4125687025c690f9e4
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2024-12-09 12:52:17 -0700

    Update src.py

--- src.py
+++ src.py
@@ -1,1 +1,1 @@
-data = {'Status': [], 'ID': [], 'Surname': [], 'Name': [], 'Gender': [], 'Age': [], 'Q1': [], 'Q2': [], 'Q3': [], 'Q4': [], 'Q5': []}
+data = {'Status': [], 'ID': [], 'Surname': [], 'Name': [], 'Gender': [], 'Age': [], 'Q1': [], 'Q2': [], 'Q3': []
